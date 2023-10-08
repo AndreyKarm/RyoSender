dd5b993b3d6fb856ac93fb2f5654d67db4a12042
AndreyKarm <karmandreii@gmail.com>
2023-10-09 00:38:22 +0100

    added image size checker and fixer
    yeap

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
 const fetch = require('node-fetch');
 const axios = require('axios');
 const fs = require('fs');
+const sharp = require('sharp');
 const cheerio = require('cheerio');
 const { Telegraf } = require('telegraf');
 const { scheduleJob } = require('node-schedule');
+const { error } = require('console');
 
 const BOT_TOKEN = '6530554561:AAF1sBxbXbJzrDDRS1N4M99Xq27PJlLRfl0';
+//const subreddit = 'Kurisutina';
 const subreddit = 'ChurchOfRyo';
-const CHAT_ID_LIST_FILE = 'chatIdList.json'; 
+// const subreddit = 'hentai';
+const CHAT_ID_LIST_FILE = 'chatIdList.json';
 
 const bot = new Telegraf(BOT_TOKEN);
 let chatIdList = [];
@@ -31,23 +35,35 @@ bot.start((ctx) => {
     if (!chatIdList.includes(chatId)) {
         chatIdList.push(chatId);
         saveChatIdList();
+        console.log(chatIdList);
     }
     ctx.reply(`Welcome to the ${subreddit} Bot! Send /getimage to get a random image from /r/${subreddit}. Or wait till 8 p.m. GMT+1 to get an image`);
     console.log(chatIdList)
 });
 
 async function getRandomImage() {
-    while (true) {
-        const response = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`);
-        const data = await response.json();
-        if (data[0].data.children[0].data.url.endsWith('.jpg') || data[0].data.children[0].data.url.endsWith('.png')) 
-        {
-            const post = data[0].data.children[0].data;
-            return {
-                imageUrl: post.url,
-                title: `${post.title}\nImage URL: ${post.url}`,
+    try {
+        while (true) {
+            const response = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`);
+            const data = await response.json();
+
+            if (data && data[0] && data[0].data && data[0].data.children && data[0].data.children.length > 0) {
+                const post = data[0].data.children[0].data;
+
+                if (post.url && (post.url.endsWith('.jpg') || post.url.endsWith('.png'))) {
+                    return {
+                        imageUrl: post.url,
+                        title: `${post.title}\nImage URL: ${post.url}`,
+                    };
+                }
             }
-}}}
+        }
+    } catch (error) {
+        console.error('Error fetching or processing the random post:', error);
+        throw error;
+    }
+}
+
 
 async function sendTopPost() {
     try {
@@ -66,6 +82,26 @@ async function sendTopPost() {
     }
 }
 
+async function resizeImage(imageUrl) {
+    try {
+        const resizedImageBuffer = await sharp({
+            create: {
+                width: 1000,
+                height: 100,
+                channels: 4, // 4 channels for RGBA
+                background: { r: 255, g: 255, b: 255, alpha: 1 }, // White background
+            },
+        })
+            .composite([{ input: imageUrl }])
+            .toBuffer();
+
+        return `data:image/jpeg;base64,${resizedImageBuffer.toString('base64')}`;
+    } catch (error) {
+        console.error('Error resizing the image:', error);
+        return null;
+    }
+}
+
 bot.command('getimage', async (ctx) => {
     try {
         const { imageUrl, title } = await getRandomImage();
@@ -80,9 +116,10 @@ bot.command('getimage', async (ctx) => {
 const job = scheduleJob('0 20 * * *', () => {
     chatIdList.forEach((chatId) => {
         sendTopPost()
-            .then(({ imageUrl, title }) => {
+            .then(async ({ imageUrl, title }) => {
+                const resizedImageUrl = await resizeImage(imageUrl);
                 bot.telegram.sendMessage(chatId, "Hey there, catch a photo and it'll make your day better :)");
-                bot.telegram.sendPhoto(chatId, { url: imageUrl }, { caption: title });
+                bot.telegram.sendPhoto(chatId, { url: resizedImageUrl }, { caption: title });
                 console.log(`Image URL (${imageUrl}) sent to chat ID: ${chatId}`);
             })
             .catch((error) => {
